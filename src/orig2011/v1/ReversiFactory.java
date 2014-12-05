c7accfdf31c440142d6e31d53ed0e94af9c00d07
Jonas Enlund <enlund.jonas@gmail.com>
2014-12-05 16:25:43 +0100

    deluppg 1

--- src/orig2011/v1/ReversiFactory.java
+++ src/orig2011/v1/ReversiFactory.java
@@ -1,18 +1,24 @@
 package orig2011.v1;
 
+import orig2011.v0.GameModel;
+import orig2011.v0.GoldModel;
+import orig2011.v0.IGameFactory;
+import orig2011.v0.ReversiModel;
+
 /**
  * Factory class for available games.
  */
-public class GameFactory implements IGameFactory {
+public class ReversiFactory implements IGameFactory {
+
 	/**
 	 * Returns an array with names of games this factory can create. Used by GUI
 	 * list availible games.
 	 */
 	@Override
 	public String[] getGameNames() {
-		return new String[] { "Gold", "Reversi" };
+		return new String[] { "Gold", "Reversi"};
 	}
-	
+
 	/**
 	 * Returns a new model object for the game corresponding to its Name.
 	 * 
@@ -29,6 +35,8 @@ public class GameFactory implements IGameFactory {
 		if (gameName.equals("Reversi")) {
 			return new ReversiModel();
 		}
+
+
 		throw new IllegalArgumentException("No such game: " + gameName);
 	}
 }
