f19df16a53b9144b1cce5ff8af0fa854677efe9c
Anton Mårtensson <anton.v.martensson@gmail.com>
2014-12-08 16:43:07 +0100

    Deluppgift 3

--- src/orig2011/v3/IGameModel.java
+++ src/orig2011/v3/IGameModel.java
@@ -1,6 +1,6 @@
-package orig2011.v2;
+package orig2011.v3;
 
-import orig2011.v0.GameOverException;
+import java.awt.Dimension;
 
 /**
  * Common superclass for all game model classes.
@@ -8,7 +8,20 @@ import orig2011.v0.GameOverException;
  * Constructors of subclasses should initiate matrix elements and additional,
  * game-dependent fields.
  */
-public interface GameModel {
+public interface IGameModel {
+	
+	/**
+	 * Returns the size of the gameboard.
+	 */
+	public Dimension getGameboardSize();
+	
+	/**
+	 * Returns the GameTile in logical position (x,y) of the gameboard.
+	 * 
+	 * @param pos
+	 *            The position in the gameboard matrix.
+	 */
+	public GameTile getGameboardState(final int x, final int y);
 	
 	/**
 	 * This method is called repeatedly so that the game can update it's state.
