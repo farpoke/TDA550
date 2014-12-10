6fe90e692bdd9464d98752aec3d2adc46090d086
Anton Mårtensson <anton.v.martensson@gmail.com>
2014-12-10 16:24:38 +0100

    GameView

---
+++ src/orig2011/v5/GameUtils.java
@@ -0,0 +1,54 @@
+package orig2011.v5;
+
+public enum GameUtils {
+	;
+	
+	/**
+	 * Set the tile on a specified position in the gameboard.
+	 * 
+	 * @param pos
+	 *            The position in the gameboard matrix.
+	 * @param tile
+	 *            The type of tile to paint in specified position
+	 */
+	public static void setGameboardState(GameTile[][] board, final Position pos, final GameTile tile) {
+		setGameboardState(board, pos.getX(), pos.getY(), tile);
+	}
+	
+	/**
+	 * Set the tile on a specified position in the gameboard.
+	 * 
+	 * @param x
+	 *            Coordinate in the gameboard matrix.
+	 * @param y
+	 *            Coordinate in the gameboard matrix.
+	 * @param tile
+	 *            The type of tile to paint in specified position
+	 */
+	public static void setGameboardState(GameTile[][] board, final int x, final int y, final GameTile tile) {
+		board[x][y] = tile;
+	}
+	
+	/**
+	 * Returns the GameTile in logical position (x,y) of the gameboard.
+	 * 
+	 * @param pos
+	 *            The position in the gameboard matrix.
+	 */
+	public static GameTile getGameboardState(GameTile[][] board, final Position pos) {
+		return getGameboardState(board, pos.getX(), pos.getY());
+	}
+	
+	/**
+	 * Returns the GameTile in logical position (x,y) of the gameboard.
+	 * 
+	 * @param x
+	 *            Coordinate in the gameboard matrix.
+	 * @param y
+	 *            Coordinate in the gameboard matrix.
+	 */
+	public static GameTile getGameboardState(GameTile[][] board, final int x, final int y) {
+		return board[x][y];
+	}
+	
+}
