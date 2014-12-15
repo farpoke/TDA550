8d7ff88178fd449cdd79a5055b130586d0efa62f
Anton Mårtensson <anton.v.martensson@gmail.com>
2014-12-15 15:33:47 +0100

    Deluppgift 6

---
+++ src/orig2011/v6/GameModel.java
@@ -0,0 +1,40 @@
+package orig2011.v6;
+
+import java.awt.Dimension;
+
+/**
+ * Common superclass for all game model classes.
+ * 
+ * Constructors of subclasses should initiate matrix elements and additional,
+ * game-dependent fields.
+ */
+public interface GameModel extends IObservable {
+	
+	/**
+	 * Returns the size of the gameboard.
+	 */
+	public Dimension getGameboardSize();
+	
+	/**
+	 * Returns the GameTile in logical position (x,y) of the gameboard.
+	 * 
+	 * @param pos
+	 *            The position in the gameboard matrix.
+	 */
+	public GameTile getGameboardState(final int x, final int y);
+	
+	/**
+	 * This method is called repeatedly so that the game can update it's state.
+	 * 
+	 * @param lastKey
+	 *            The most recent keystroke.
+	 */
+	public void gameUpdate(int lastKey) throws GameOverException;
+	
+        /**
+         * Return the desired delay between update calls, or a value
+         * less than zero if updates should be triggered by user input.
+         */
+        public int getUpdateSpeed();
+        
+}
